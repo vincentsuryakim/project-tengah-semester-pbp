0d86d2273e7be802057ce2a17bd08e492b1cd201
Pramesti Anjani Putri <tianpramesti01@gmail.com>
2021-10-25 00:39:19 +0700

    Edit Navbar

--- edukasiProtokol/models.py
+++ edukasiProtokol/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db.models.fields import DateField
 
-# Create your models here.
+class Artikel(models.Model):
+    Judul = models.CharField(max_length=300)
+    Tanggal_publish = models.DateField
+    Penulis = models.CharField(max_length=300)
+    Isi = models.TextField()
