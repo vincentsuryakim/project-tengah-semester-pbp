2ba68e7929e93503c08f47dd1c310a057511761d
Vincent Suryakim <vincentsuryakim@gmail.com>
2021-10-27 23:55:19 +0700

    formatted string

--- sebaran/views.py
+++ sebaran/views.py
@@ -12,10 +12,10 @@ def index(request):
     response = {
         'sebaran': Sebaran.objects.all().order_by('-jumlah_kasus_aktif'),
         'sebaran_values': str(list(Sebaran.objects.order_by('-jumlah_kasus_aktif').values())),
-        'terkonfirmasi': Sebaran.objects.aggregate(Sum('jumlah_kasus_terkonfirmasi'))['jumlah_kasus_terkonfirmasi__sum'],
-        'aktif': Sebaran.objects.aggregate(Sum('jumlah_kasus_aktif'))['jumlah_kasus_aktif__sum'],
-        'sembuh': Sebaran.objects.aggregate(Sum('jumlah_sembuh'))['jumlah_sembuh__sum'],
-        'meninggal': Sebaran.objects.aggregate(Sum('jumlah_meninggal'))['jumlah_meninggal__sum'],
+        'terkonfirmasi': f"{Sebaran.objects.aggregate(Sum('jumlah_kasus_terkonfirmasi'))['jumlah_kasus_terkonfirmasi__sum']:,}".replace(",", "."),
+        'aktif': f"{Sebaran.objects.aggregate(Sum('jumlah_kasus_aktif'))['jumlah_kasus_aktif__sum']:,}".replace(",", "."),
+        'sembuh': f"{Sebaran.objects.aggregate(Sum('jumlah_sembuh'))['jumlah_sembuh__sum']:,}".replace(",", "."),
+        'meninggal': f"{Sebaran.objects.aggregate(Sum('jumlah_meninggal'))['jumlah_meninggal__sum']:,}".replace(",", "."),
         'provinsi': Sebaran.objects.values('provinsi').order_by(Lower('provinsi'))
     }
 
