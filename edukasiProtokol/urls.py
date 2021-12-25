9fe8460e2c180b8e54a22aeba05a6973e484447d
Pramesti Anjani Putri <tianpramesti01@gmail.com>
2021-12-25 20:37:20 +0700

    Update views and urls

--- edukasiProtokol/urls.py
+++ edukasiProtokol/urls.py
@@ -1,9 +1,11 @@
 from django.urls.conf import path
-from .views import buat_artikel, index, next_artikel
+from .views import buat_artikel, index, next_artikel, get_data, add_data
 
 urlpatterns = [
     path('', index, name='index_edukasi'),
     path('buat-artikel', buat_artikel, name='buat_artikel'),
-    path('next-artikel', next_artikel, name='next_artikel')
+    path('next-artikel', next_artikel, name='next_artikel'),
+    path('get-data', get_data, name='get_data'),
+    path('add-data', add_data, name='add_data')
 ]
 
